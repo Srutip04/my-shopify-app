b132b86be4299467a35ce28b1548d54c5db73dbf
Srutip04 <subuhishruti75@gmail.com>
2023-05-26 23:37:06 +0530

    done!

---
+++ src/components/table/Table.jsx
@@ -0,0 +1,145 @@
+import React, { useState } from "react";
+import "./table.scss";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import { Select, MenuItem, Button } from "@mui/material";
+
+const List = () => {
+  const initialRows = [
+    {
+      id: 1143155,
+      product: "Acer Nitro 5",
+      img: "https://m.media-amazon.com/images/I/81bc8mA3nKL._AC_UY327_FMwebp_QL65_.jpg",
+      customer: "John Smith",
+      date: "12 March",
+      amount: 785,
+      method: "Cash on Delivery",
+      status: "Approved",
+    },
+    {
+      id: 2235235,
+      product: "Playstation 5",
+      img: "https://m.media-amazon.com/images/I/31JaiPXYI8L._AC_UY327_FMwebp_QL65_.jpg",
+      customer: "Michael Doe",
+      date: "1 March",
+      amount: 900,
+      method: "Online Payment",
+      status: "Pending",
+    },
+    {
+      id: 2342353,
+      product: "Redragon S101",
+      img: "https://m.media-amazon.com/images/I/71kr3WAj1FL._AC_UY327_FMwebp_QL65_.jpg",
+      customer: "John Smith",
+      date: "1 March",
+      amount: 35,
+      method: "Cash on Delivery",
+      status: "Pending",
+    },
+    {
+      id: 2357741,
+      product: "Razer Blade 15",
+      img: "https://m.media-amazon.com/images/I/71wF7YDIQkL._AC_UY327_FMwebp_QL65_.jpg",
+      customer: "Jane Smith",
+      date: "10 March",
+      amount: 920,
+      method: "Online",
+      status: "Approved",
+    },
+    {
+      id: 2342355,
+      product: "ASUS ROG Strix",
+      img: "https://m.media-amazon.com/images/I/81hH5vK-MCL._AC_UY327_FMwebp_QL65_.jpg",
+      customer: "Harold Carol",
+      date: "1 March",
+      amount: 2000,
+      method: "Online",
+      status: "Pending",
+    },
+  ];
+
+  const [rows, setRows] = useState(initialRows);
+  const [filterOption, setFilterOption] = useState("");
+
+  const handleFilter = () => {
+    let filteredRows = initialRows;
+
+    if (filterOption === "date") {
+      // Filter based on date
+      filteredRows = initialRows.filter(
+        (row) => row.date === "YOUR_DATE_FILTER"
+      );
+    } else if (filterOption === "status") {
+      // Filter based on status
+      filteredRows = initialRows.filter(
+        (row) => row.status === "YOUR_STATUS_FILTER"
+      );
+    }
+
+    setRows(filteredRows);
+  };
+
+  const handleReset = () => {
+    setRows(initialRows);
+    setFilterOption("");
+  };
+
+  const handleFilterOptionChange = (event) => {
+    setFilterOption(event.target.value);
+  };
+  return (
+    <div>
+      <div>
+        <Select value={filterOption} onChange={handleFilterOptionChange}>
+          <MenuItem value="">Select Filter Option</MenuItem>
+          <MenuItem value="date">Filter by Date</MenuItem>
+          <MenuItem value="status">Filter by Status</MenuItem>
+        </Select>
+        <Button onClick={handleFilter}>Filter</Button>
+        <Button onClick={handleReset}>Reset</Button>
+      </div>
+      <TableContainer component={Paper} className="table">
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell className="tableCell">Tracking ID</TableCell>
+              <TableCell className="tableCell">Product</TableCell>
+              <TableCell className="tableCell">Customer</TableCell>
+              <TableCell className="tableCell">Date</TableCell>
+              <TableCell className="tableCell">Amount</TableCell>
+              <TableCell className="tableCell">Payment Method</TableCell>
+              <TableCell className="tableCell">Status</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow key={row.id}>
+                <TableCell className="tableCell">{row.id}</TableCell>
+                <TableCell className="tableCell">
+                  <div className="cellWrapper">
+                    <img src={row.img} alt="" className="image" />
+                    {row.product}
+                  </div>
+                </TableCell>
+                <TableCell className="tableCell">{row.customer}</TableCell>
+                <TableCell className="tableCell">{row.date}</TableCell>
+                <TableCell className="tableCell">{row.amount}</TableCell>
+                <TableCell className="tableCell">{row.method}</TableCell>
+                <TableCell className="tableCell">
+                  <span className={`status ${row.status}`}>{row.status}</span>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
+  );
+};
+
+export default List;
